0e301187384bef7af6de8be757be3d22f81a04a8
Filip Kováč <kovac.filip.2018@skola.ssps.cz>
2021-04-18 21:53:25 +0200

    Přidejte soubory projektu.

---
+++ WpfApp7/Class1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace WpfApp7
+{
+    class Person : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+
+        private string name;
+        public string Name
+        {
+            get => name;
+            set { Name = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Name")); }
+        }
+
+
+
+        private int date;
+        public int Date
+        {
+            get => date;
+            set { date = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Date")); }
+        }
+
+
+
+        private string identification_number;
+        public string Identification_number
+        {
+            get => identification_number;
+            set { identification_number = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Identification Number")); }
+        }
+
+
+
+
+
+        public static Dictionary<string, Person> AllPersons = new Dictionary<string, Person>();
+
+
+
+        public static void InitPersons()
+        {
+            AllPersons["Filip Kováč"] = new Person
+            {
+                Name = "Filip Kováč",
+                Date = 2003,
+                Identification_number = "xxxxx",
+            };
+
+
+
+            AllPersons["Pavel Novák"] = new Person
+            {
+                Name = "Pavel Novák",
+                Date = 2000,
+                Identification_number = "xxxxx",
+            };
+
+
+
+            AllPersons["Cornell"] = new Person
+            {
+                Name = "Luboš Soukup",
+                Date = 1998,
+                Identification_number = "xxxxx",
+            };
+        }
+    }
+}
+}
---
+++ WpfApp7/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp7
+{
+    /// <summary>
+    /// Interakční logika pro MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Person.InitPersons();
+            Persons.DataContext = Person.AllPersons;
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Persons_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Person p = ((KeyValuePair<string, Person>)(sender as ListView).SelectedItem).Value;
+            DataContext = p;
+        }
+
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Persons.SelectedIndex = 0;
+        }
+    }
+    }
+}
